7a136cea69378c5735bb002486755f51aad0f01c
Jesper Persson <jesperpersson96@hotmail.se>
2024-10-25 11:58:25 +0200

    vi det blir för avancerad för oss när vu börjar coda med saker i javascript vi inte gått igenom. vi ändrade lite o kommer göra på ett annat sätt hoppas det går bra.

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,45 @@
-alphbet();
+const words = [
+  "javascript",
+  "python",
+  "java",
+  "react",
+  "vue",
+  "angular",
+  "html",
+  "mongodb",
+];
 
-// Generera alfabet som kanappar
-function alphbet() {
-  const alphabetDiv = document.querySelector("#alphabetDiv");
-  const letters = "abcdefghijklmnopqrstuvwxyz";
+let letterbuttons = document.querySelectorAll(button);
 
-  console.log(letters.split(""));
+letterbuttons.addEventListener("click", () => {
+  console.log(letterbuttons);
+});
 
-  letters.split("").forEach((letter) => {
-    const button = document.createElement("button");
-    button.textContent = letter;
-    button.classList.add("letter-button");
+// alphbet();
 
-    button.addEventListener("click", () => {
-      console.log(`Klickade på: ${letter}`);
-      //   en funktion som hämtar upp värdetoch läggs till sin egen funktion för jämförelse
-    });
+// // Generera alfabet som kanappar
+// function alphbet() {
+//   const alphabetDiv = document.querySelector("#alphabetDiv");
+//   const letters = "abcdefghijklmnopqrstuvwxyz".toUpperCase(); //ändrade så det blec stora bokstäver
 
-    alphabetDiv.appendChild(button);
-  });
-}
+//   console.log(letters.split(""));
+
+//   letters.split("").forEach((letter) => {
+//     const button = document.createElement("button");
+//     button.textContent = letter;
+//     button.classList.add("letter-button");
+
+//     button.addEventListener("click", () => {
+//       console.log(`Klickade på: ${letter}`);
+//       //   en funktion som hämtar upp värdetoch läggs till sin egen funktion för jämförelse
+//       handleGuess(letter, button);
+//     });
+
+//     alphabetDiv.appendChild(button);
+//   });
+// }
+
+// // Hantera en gissning - Funktion som jämför gissade bokstäver mot ordet
+// function handleGuess(letter, button) {
+//   console.log("handleguess funktionen syns");
+// }
