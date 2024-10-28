efaf71077021152a2f731decd859b5660e9cc5d6
Jesper Persson <jesperpersson96@hotmail.se>
2024-10-28 12:23:43 +0100

    litegrhor

--- javascript/script.js
+++ javascript/script.js
@@ -13,17 +13,15 @@ let wrongGuesses = [];
 let attempts = 6;
 let randomWord;
 
-
 const display = document.querySelector(".display");
 const countGuess = document.querySelector(".count-guess");
 const btnAgain = document.querySelector(".again");
+countGuess.textContent = attempts;
+display.vlaue = guessedLetters;
 
-function reset() {
-
-}
+function reset() {}
 
 function startGame() {
-
   // Väljer ett slumpmässigt ord från listan
   randomWord = words[Math.floor(Math.random() * words.length)];
   console.log(randomWord.split(""));
@@ -38,16 +36,15 @@ function startGame() {
 function displayWord() {
   // Uppdaterar displayfältet med understrykningar med antal bokstäver i ett ord
   display.value = guessedLetters.join(" ");
-
 }
 
 // Event till en lyssnare för tangenttryckningar
 document.addEventListener("keydown", (event) => {
   const letter = event.key.toLowerCase();
-  
+
   // Här kan man kontrollera att inmatningen är en boksatv
 
-  console.log(letter); 
+  console.log(letter);
 });
 
 function handleGuess() {}
@@ -59,6 +56,5 @@ function updateCorrectGusses() {}
 function checkGameStatus() {}
 
 function endGame() {}
-
 
 startGame();
